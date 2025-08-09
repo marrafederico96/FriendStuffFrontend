fd6c2b0e51045c373d557a5514a2aace6e393459
Federico Marra <marrafederico@proton.me>
2025-08-09 19:28:54 +0200

    update expense feature

--- src/app/dto/expenseEventDto.ts
+++ src/app/dto/expenseEventDto.ts
@@ -1,12 +1,13 @@
 export interface ExpenseEventDto {
-    expenseName: string,
-    amount: number,
-    payerUsername: string,
-    eventName: string,
+    expenseName: string;
+    amount: number;
+    payerUsername: string;
+    eventName: string;
     expenseParticipant?: ExpenseParticipantDto[]
 }
 
 export interface ExpenseParticipantDto {
     userName: string;
     eventName?: string;
+    amountOwed?: number;
 }
